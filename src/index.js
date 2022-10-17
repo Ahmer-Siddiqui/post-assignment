dd77d3239a5f449aa4fa62cda8bd2dd7c26fdcfd
Ahmer <siddiquiahmer970@gmail.com>
2022-10-17 21:47:47 +0500

    editing post

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const Profile = ({ title, image, text }) => (
 
 
 const Page = () => (
-  <div>
+  <div className='main'>
     <Profile
       title="Elon Musk"
       image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/220px-Elon_Musk_Royal_Society_%28crop2%29.jpg"
@@ -25,16 +25,16 @@ const Page = () => (
       />
     
     
-    {/* <Profile
-      title="Winning the Best Music Director Award"
-      text="The Filmfare Awards are recognised as India’s foremost awards and one of the oldest awards for Bollywood movies, presented on a yearly basis by The Times Group. Originally named the Clare Awards after The Times of India film critic Clare Mendonca. The first event held on March 21, 1954 gave out only five awards. Best Film Award was won by Do Bigha Zameen (1953), which also won its director Bimal Roy the Best Director Award. Dilip Kumar won for his acting in Daag (1952) while Meena Kumari won Best Actor (Female) for Baiju Bawra (1952). Naushad Ali’s music for Baiju Bawra won him the Best Music Director Award. These winners were selected by polling readers of the Filmfare magazine."
-      image="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+     <Profile
+      title="Bill Gates"
+      text="Gates was born and raised in Seattle, Washington. In 1975, he and Allen founded Microsoft in Albuquerque, New Mexico. It became the world's largest personal computer software company.[5][a] Gates led the company as chairman and CEO until stepping down as CEO in January 2000, succeeded by Steve Ballmer, but he remained chairman of the board of directors and became chief software architect.[8] During the late 1990s, he was criticized for his business tactics, which have been considered anti-competitive. This opinion has been upheld by numerous court rulings.[9] In June 2008, Gates transitioned to a part-time role at Microsoft and full-time work at the Bill & Melinda Gates Foundation, the private charitable foundation he and his then-wife, Melinda Gates, established in 2000.[10] He stepped down as chairman of the board of Microsoft in February 2014 and assumed a new post as technology adviser to support the newly appointed CEO Satya Nadella.[11] In March 2020, Gates left his board positions at Microsoft and Berkshire Hathaway to focus on his philanthropic efforts on climate change, global health and development, and education.[12]."
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Bill_Gates_2017_%28cropped%29.jpg/220px-Bill_Gates_2017_%28cropped%29.jpg"
     />
     <Profile
-      title="best drama Awards"
-      text="The Filmfare Awards are recognised as India’s foremost awards and one of the oldest awards for Bollywood movies, presented on a yearly basis by The Times Group. Originally named the Clare Awards after The Times of India film critic Clare Mendonca. The first event held on March 21, 1954 gave out only five awards. Best Film Award was won by Do Bigha Zameen (1953), which also won its director Bimal Roy the Best Director Award. Dilip Kumar won for his acting in Daag (1952) while Meena Kumari won Best Actor (Female) for Baiju Bawra (1952). Naushad Ali’s music for Baiju Bawra won him the Best Music Director Award. These winners were selected by polling readers of the Filmfare magazine."
-      image="https://images.squarespace-cdn.com/content/v1/5b398fb2f793925040070b55/1569358740365-XHKY3VFY8H86Q130T9MP/performers+on+stage?format=1000w"
-    /> */}
+      title="jeff Bezos"
+      text="Jeffrey Preston Bezos (/ˈbeɪzoʊs/ BAY-zohss;[2] né Jorgensen; born January 12, 1964) is an American entrepreneur, media proprietor, investor, computer engineer, and commercial astronaut.[3][4] He is the founder, executive chairman, and former president and CEO of Amazon. With a net worth of around US$139 billion as of October 2022, Bezos is the third-wealthiest person in the world and was the wealthiest from 2017 to 2021 according to both Bloomberg's Billionaires Index and Forbes.[5][6]"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Jeff_Bezos_at_Amazon_Spheres_Grand_Opening_in_Seattle_-_2018_%2839074799225%29_%28cropped%29.jpg/220px-Jeff_Bezos_at_Amazon_Spheres_Grand_Opening_in_Seattle_-_2018_%2839074799225%29_%28cropped%29.jpg"
+    />
   </div>
 )
 
